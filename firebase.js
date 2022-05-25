b27d928781439b5b3c06b2821a4c85441ac66a7f
azzy kesuma <official.azzy@gmail.com>
2022-05-25 08:27:12 +0700

    initial commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDmjUT-EIB08W5TmjRaGKCxnhSYJ1NiYdo",
+  authDomain: "next-auth-crud-71378.firebaseapp.com",
+  projectId: "next-auth-crud-71378",
+  storageBucket: "next-auth-crud-71378.appspot.com",
+  messagingSenderId: "655329275036",
+  appId: "1:655329275036:web:632292e82e03edd15ad159"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ layout/Index.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export const Layout = (props) => {
+    <div>
+        <Link href='/'>Home</Link>
+        <Link href='/register'>Register</Link>
+        <Link href='/login'>Login</Link>
+    </div>
+}
